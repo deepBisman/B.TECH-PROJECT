c92e55c10f342a6b0ee8baf5a019a2b3ed9a42cf
deepBisman <bismandeepsignh0012@gmail.com>
2024-07-16 06:25:46 +0530

    Add Readme Extest

--- external_tests/download_extest_dataset.sh
+++ external_tests/download_extest_dataset.sh
@@ -14,8 +14,8 @@ mkdir -p $DATASET_DIR
 
 # Download the dataset from Kaggle
 echo "Downloading dataset from Kaggle..."
-kaggle datasets download -d deeparora12/rgb-india -p "$DATASET_DIR/rgb_test_images" --unzip
-kaggle datasets download -d deeparora12/height-india-2 -p "$DATASET_DIR/target_test_images" --unzip
+kaggle datasets download -d deeparora12/rgb-test-dataset -p "$DATASET_DIR/rgb_test_images" --unzip
+kaggle datasets download -d deeparora12/target-test-dataset -p "$DATASET_DIR/target_test_images" --unzip
 
 # Complete
 echo -e "\e[94mAll datasets created successfully\e[0m"
